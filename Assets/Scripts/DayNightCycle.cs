79fa93e7fef257a4fe1991c4edf01a755414d54a
OIF <hwaga0714@gmail.com>
2023-04-19 09:30:42 +0900

    끝

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField]
+    private Gradient gradient;
+
+    [SerializeField]
+    private float secondsPerDay = 10f;
+
+    private float dayTime;
+    private float dayTimeSpeed;
+
+    private Light2D light2D;
+
+    private void Awake()
+    {
+        light2D = GetComponent<Light2D>();
+        dayTimeSpeed = 1f / secondsPerDay;
+    }
+
+    private void Update()
+    {
+        dayTime += Time.deltaTime * dayTimeSpeed;
+        light2D.color = gradient.Evaluate(dayTime % 1f);
+    }
+}
